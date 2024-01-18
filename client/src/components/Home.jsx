bd0d10bdb407f5198758453e2ff3816fe384f4d1
Tejas <tejasdalvi124b@gmail.com>
2024-01-18 23:56:51 +0530

    created frontend for homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,10 @@
 // import { parseEther } from 'viem';
 // import { useToken } from 'wagmi'
 import { useEffect } from 'react';
+import { motion } from 'framer-motion'
 // import { useBalance } from 'wagmi';
+import './Home.css'
+import vase2 from '../assets/vase 2 nobg.png'
 
 
 function Home({address}) {
@@ -39,10 +42,44 @@ function Home({address}) {
   // },[GHOInfo])
 
   return (
-    <div>
-    <div>Home</div> 
-    {/* <div>{balance?.formatted}{balance?.symbol}</div> */}
-    {/* <button onClick={()=>{write()}} disabled={!address}>{address ? "Click" : "disabled"}</button> */}
+    <div className='home'>
+    <div className='home-left'>
+      <div className='home-heading'>
+        <motion.h1
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ ease: "easeInOut", duration: 1.5 , times: [0.3]}} 
+        >Let&apos;s Gho Eco</motion.h1>
+
+        <motion.h2
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ ease: "easeInOut", duration: 1.5 , times: [0.6]}} 
+        >Where Green Meets</motion.h2>
+
+        <motion.h3
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ ease: "easeInOut", duration: 1.5 , times: [0.9]}}  
+        >Blockchain</motion.h3>
+
+        <motion.p
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ ease: "easeInOut", duration: 1.5 , times: [0.9]}} 
+        >GHO-ECO offers a visionary platform where organizations showcase their environmentally conscious initiatives, Join us as we redefine the landscape of eco-conscious solutions.</motion.p>
+      </div>
+    </div> 
+
+    <div className='home-right'>
+      <motion.div className='flowers'
+      initial={{ opacity: 0 , x: 100 }}
+      animate={{ opacity: 1 , x: 0 }}
+      transition={{ ease: "easeInOut", duration: 2 , times: [0.3]}} 
+      >
+        <img src={vase2}></img>
+      </motion.div>
+    </div> 
     </div>
   )
 }
--- client/src/components/Participate.jsx
+++ client/src/components/Participate.jsx
@@ -2,19 +2,46 @@
 import "./Participate.css";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import axios from "axios";
 
 function Participate({ address }) {
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
   const [description, setDescription] = useState("");
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("file", image);
+
+    const resFile = await axios({
+      method: "post",
+      url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+      data: formData,
+      headers: {
+        pinata_api_key: `840f25a841a032f15eb3`,
+        pinata_secret_api_key: `feca959005f2d80a1aedc6d731d80fe05d18a011576f47d2cbe660e62b47430d`,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    const ImgHash = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+
     setName("");
     setImage("");
     setDescription("");
     alert(name + " " + description + " " + image);
   }
+
+//   const retrieveFile=(e)=>{
+//     e.preventDefault(); 
+//     const data = e.target.files[0]; 
+//     const reader = new window.FileReader(); 
+//     reader.readAsArrayBuffer(data); 
+//     reader.onloadend=()=>{
+//         setImage(e.target.files[0]); 
+//     }
+//     }; 
 
   return !address ? (
     "Please Connect Your Wallet"
@@ -36,7 +63,7 @@ function Participate({ address }) {
             onChange={(e) => {
               setName(e.target.value);
             }}
-            className="noCircle field"
+            className="field"
           />
 
           <textarea
@@ -47,7 +74,7 @@ function Participate({ address }) {
             onChange={(e) => {
               setDescription(e.target.value);
             }}
-            className="message-box noCircle field"
+            className="message-box field"
           ></textarea>
 
           <div className="img-input">
@@ -55,10 +82,7 @@ function Participate({ address }) {
               required
               value={image}
               type="file"
-              accept="image/*"
-              onChange={(e) => {
-                setImage(e.target.value);
-              }}
+              onChange={(e)=>{setImage(e.target.value)}}
             />
           </div>
 
